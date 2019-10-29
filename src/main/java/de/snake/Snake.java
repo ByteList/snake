5099c82e164a159ae37f11baf12531d9013e63a2
ByteList <n.emmrich@wrba.ch>
2019-10-29 12:11:29 +0100

    Init repo

---
+++ src/main/java/de/snake/Snake.java
@@ -0,0 +1,15 @@
+package de.snake;
+
+/**
+ * Created by Niklas Emmrich on 29.10.2019.
+ * <p>
+ * Copyright by Niklas Emmrich.
+ */
+public class Snake {
+
+    private static Snake instance;
+
+    public Snake() {
+        instance = this;
+    }
+}
---
+++ src/main/java/de/snake/bootstrap/SnakeLauncher.java
@@ -0,0 +1,19 @@
+package de.snake.bootstrap;
+
+import de.snake.Snake;
+
+/**
+ * Created by Niklas Emmrich on 29.10.2019.
+ * <p>
+ * Copyright by Niklas Emmrich.
+ */
+public class SnakeLauncher {
+
+    public static void main(String[] args) {
+        System.out.println("Initializing Snake...");
+
+        Snake snake = new Snake();
+
+
+    }
+}
